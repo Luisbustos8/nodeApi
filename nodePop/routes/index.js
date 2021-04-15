aaa43e7bc1926fb28b9ae1e1e72340ece683fed0
Luis A. Bustos Lucas <luisabustoslucas@gmail.com>
2021-04-15 13:39:39 +0200

    Creando mis primeras rutas de prueba

--- nodePop/routes/index.js
+++ nodePop/routes/index.js
@@ -6,4 +6,45 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+//Solo peticiones GET
+router.get('/parametro/:dato', (req, res, next) => {
+  const dato = req.params.dato;
+  console.log(req.params);
+
+  res.send('He recibido el dato:' + dato)
+});
+
+router.get('/parametrofiltrado/:dato([0-9]+)', (req, res, next) => {
+  const dato = req.params.dato;
+  console.log(req.params);
+  res.send('He recibido el dato:' + dato);
+});
+
+router.get('/parametrofiltrado/:dato?', (req, res, next) => {
+  const dato = req.params.dato;
+  console.log(req.params);
+  res.send('He recibido el dato:' + dato);
+});
+
+router.get('/parametros/:dato/piso/:piso/puerta/:puerta', (req, res, next) => {
+  const dato = req.params.dato;
+  console.log(req.params);
+  res.send('He recibido el dato:' + dato);
+});
+
+// Recibir dato Query String 
+// /querystring?dato=20
+router.get('/querystring', (req, res, next) => {
+  const dato = req.query.dato;
+  console.log(req.query);
+  res.send('He recibido el dato:' + dato );
+})
+
+router.post('/enelbody', (req, res, next) => {
+  console.log(req.body);
+  console.log('Headers:', req.get('Content-type'))
+  res.send('He recibido el dato:' + req.body.numero );
+})
+
 module.exports = router;
